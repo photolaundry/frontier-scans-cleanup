1525be665c27b225e3105fa3b77a5fafabd62027
Bobo Li <bobo@photolaundrysf.com>
2024-01-27 02:43:59 -0800

    [refactor] renamed variables for clarity in both cleanup scripts

--- frontier_scans_cleanup/cleanup_c4c5.py
+++ frontier_scans_cleanup/cleanup_c4c5.py
@@ -22,8 +22,9 @@ from wand.image import Image
 class FrontierCleanerC4C5:
     EXIF_DATETIME_STR_FORMAT = "%Y:%m:%d %H:%M:%S"
     EXIFTOOL_SUCCESSFUL_WRITE_MESSAGE = "1 image files updated"
-    IMAGE_DIR_PATTERN = r"(?P<order_id>.{1,10})(?P<roll_number>\d{6})"
-    IMAGE_DIR_GLOB_PATTERN = "*" + "[0-9]" * 6
+    
+    ROLL_DIR_PATTERN = r"(?P<order_id>.{1,10})(?P<roll_number>\d{6})"
+    ROLL_DIR_GLOB_PATTERN = "*" + "[0-9]" * 6
     IMAGE_NAME_PATTERN = r"(?P<frame_number>\d{6})"
 
     def __init__(
@@ -60,16 +61,16 @@ class FrontierCleanerC4C5:
         self.image_name_matcher = re.compile(self.IMAGE_NAME_PATTERN)
 
     def clean(self):
-        for image_dir in self.find_all_image_dirs():
+        for roll_dir in self.find_all_rolls():
             try:
-                self.convert_bmps_to_tifs(image_dir)
-                self.fix_timestamps(image_dir)
-                self.rename_images(image_dir)
+                self.convert_bmps_to_tifs(roll_dir)
+                self.fix_timestamps(roll_dir)
+                self.rename_images(roll_dir)
             except ValueError as e:
                 print(e)
-                print(f"skipping directory {image_dir}...")
+                print(f"skipping directory {roll_dir}...")
 
-    def find_all_image_dirs(self):
+    def find_all_rolls(self):
         """
         The Frontier exports images into a dir for each roll, where the
         directory name is the order id (up to 10 of any character) followed by
@@ -82,20 +83,20 @@ class FrontierCleanerC4C5:
         found_dirs += sorted(
             filter(
                 lambda x: x.is_dir(),
-                self.frontier_export_path.glob(self.IMAGE_DIR_GLOB_PATTERN),
+                self.frontier_export_path.glob(self.ROLL_DIR_GLOB_PATTERN),
             )
         )
 
         return found_dirs
 
-    def convert_bmps_to_tifs(self, images_dir):
+    def convert_bmps_to_tifs(self, roll_dir):
         """
         Converts all BMP files to compressed TIF files (with lzw compression).
-        images_dir is a path object that represents the directory of images to
+        roll_dir is a path object that represents the directory of images to
         operate on
         """
         print("converting bmps to tifs...")
-        for image_path in sorted(images_dir.glob("*")):
+        for image_path in sorted(roll_dir.glob("*")):
             _ = image_path.stem  # the filename without extension
             suffix = image_path.suffix  # the extension including the .
 
@@ -112,35 +113,35 @@ class FrontierCleanerC4C5:
             # delete original bmps
             image_path.unlink()
 
-    def rename_images(self, images_dir):
+    def rename_images(self, roll_dir):
         """
-        Renames the images in the images_dir directory in the format:
+        Renames the images in the roll_dir directory in the format:
             R{roll_number}F{frame_number}.jpg (or .tif)
 
         Since the C4/C5 software doesn't seem to save the frame names as read
         by the DX code reader, its filenames are always frame numbers. We can
         rename these filenames to be sequential since the Frontier sometimes
         numbers these wrong, skipping frames 02-06 often.
-        images_dir is a path object that represents the directory of images to
+        roll_dir is a path object that represents the directory of images to
         operate on
         """
         images_glob = sorted(
             itertools.chain(
-                images_dir.glob("*.jpg"),
-                images_dir.glob("*.bmp"),
-                images_dir.glob("*.JPG"),
-                images_dir.glob("*.BMP"),
+                roll_dir.glob("*.jpg"),
+                roll_dir.glob("*.bmp"),
+                roll_dir.glob("*.JPG"),
+                roll_dir.glob("*.BMP"),
             )
         )
         if not images_glob:
-            print(f"No images found, skipping: {images_dir}")
+            print(f"No images found, skipping: {roll_dir}")
             return
 
         # the roll number can be extracted from the directory name
-        dir_match = re.match(self.IMAGE_DIR_PATTERN, images_dir.name)
+        dir_match = re.match(self.ROLL_DIR_PATTERN, roll_dir.name)
         if not dir_match:
             raise ValueError(
-                f"image dir doesn't match expected format: {images_dir}"
+                f"image dir doesn't match expected format: {roll_dir}"
             )
         roll_number = dir_match.group("roll_number")
         # convert roll number to an int, and then zero pad it as desired
@@ -196,13 +197,13 @@ class FrontierCleanerC4C5:
             image_path.rename(new_filepath)
 
         if self.reorg:
-            # delete the images_dir now that all images are renamed and moved
+            # delete the roll_dir now that all images are renamed and moved
             try:
-                images_dir.rmdir()
+                roll_dir.rmdir()
             except OSError:
-                print(f"Directory not empty, skipping deletion: {images_dir}")
+                print(f"Directory not empty, skipping deletion: {roll_dir}")
 
-    def fix_timestamps(self, images_dir):
+    def fix_timestamps(self, roll_dir):
         """
         Adds the DateTimeOriginal EXIF tag to all images, based on the
         filesystem modified timestamp of the file. This fixes the issue where
@@ -227,19 +228,19 @@ class FrontierCleanerC4C5:
         frames get out of order, because LR would have to use filename to sort
         since they'd all have the same capture time.
 
-        images_dir is a path object that represents the directory of images to
+        roll_dir is a path object that represents the directory of images to
         operate on.
         """
         first_image_mtime = None
         image_num = 0
         images_glob = sorted(
             itertools.chain(
-                images_dir.glob("**/*.jpg"),
-                images_dir.glob("**/*.tif"),
-                images_dir.glob("**/*.bmp"),
-                images_dir.glob("**/*.JPG"),
-                images_dir.glob("**/*.TIF"),
-                images_dir.glob("**/*.BMP"),
+                roll_dir.glob("**/*.jpg"),
+                roll_dir.glob("**/*.tif"),
+                roll_dir.glob("**/*.bmp"),
+                roll_dir.glob("**/*.JPG"),
+                roll_dir.glob("**/*.TIF"),
+                roll_dir.glob("**/*.BMP"),
             )
         )
         for image_path in images_glob:
